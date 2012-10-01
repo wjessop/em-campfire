e3bfb1f21e18e40edb0afe52cccf364e4f60db17
Will Jessop <will@willj.net>
2012-10-01 20:27:54 +0100

    Use a hash in an ivar rather than subclass Hash

--- lib/em-campfire/cache.rb
+++ lib/em-campfire/cache.rb
@@ -1,20 +1,24 @@
 module EventMachine
   class Campfire
-    class Cache < Hash
+    class Cache
       def get(key)
         key = cache_key_for(key)
-        yield self[key] if block_given?
-        self[key]
+        yield store[key] if block_given?
+        store[key]
       end
 
       def set(key, val)
-        self[cache_key_for(key)] = val
+        store[cache_key_for(key)] = val
       end
 
       private
 
       def cache_key_for(object)
         "em-campfire-#{object}"
+      end
+
+      def store
+        @store ||= {}
       end
     end
   end
